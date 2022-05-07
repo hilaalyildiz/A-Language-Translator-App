662b73fbcb6bdb35a1f3c14cf73883935f00847d
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-07 17:18:01 +0300

    A Language Translator App

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const selectTag = document.querySelectorAll("select");
+
+selectTag.forEach((tag,id) => {
+    for (const country_code in countries) {
+        // seelcting English by default as from language and Turkish as to language
+        let selected;
+        if(id == 0 && country_code== "en-GB") {
+            selected = "selected";
+        } else if(id == 1 && country_code== "tr-TR") {
+            selected = "selected";
+        }
+
+        let option = `<option value="${country_code}"${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); // adding options tag inside select tag 
+    }
+
+});
