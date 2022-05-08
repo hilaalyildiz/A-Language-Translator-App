950dfae3e5600a21c88c087a6d3c40bef824762e
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-08 15:22:34 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag,id) => {
     for (const country_code in countries) {
@@ -14,4 +17,18 @@ selectTag.forEach((tag,id) => {
         tag.insertAdjacentHTML("beforeend", option); // adding options tag inside select tag 
     }
 
+});
+
+translateBtn.addEventListener("click",() => {
+    let text = fromText.value, 
+    translateFrom = selectTag[0].value, // getting fromSelect tag value
+    translateTo = selectTag[1].value; // getting toSelect tag value
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    
+    // fetching api responseand returning it with parsing into js obj 
+    // and in another then method receiving that obj
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value =  data.responseData.translatedText;
+    })
 });
